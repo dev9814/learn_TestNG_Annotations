747f7b14e1aaa334eb0f7d1717de3e35a4594354
dev9814 <devdutt.software@gmail.com>
2024-06-18 20:51:44 +0530

    add first file

---
+++ src/test/java/annotation/TestNg/Simple_FirstNG.java
@@ -0,0 +1,13 @@
+package annotation.TestNg;
+
+import org.testng.annotations.Test;
+
+public class Simple_FirstNG {
+
+	@Test
+	public void method()
+	{
+		System.out.println("create first unit TestNG project.");
+	}
+	
+}
